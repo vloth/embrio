744c64be94134c524bd6f52a9adac8a423601b82
vloth <fran.fhb@gmail.com>
2020-06-16 09:47:38 +0200

    ϋ refactor: router on the top

--- typescript/web-boilerplate/src/todo/router.ts
+++ typescript/web-boilerplate/src/todo/router.ts
@@ -1,24 +1,26 @@
+import { Context } from 'koa'
 import * as Router from 'koa-router'
 import { decodeP } from '@infra/codec/decode'
 import { logger } from '@infra/logger'
 import { Todo, isDue } from './Todo'
 
 export const router = new Router({ prefix: '/api/todo' })
+  .get('/', getTodo)
+  .post('/', postTodo)
 
-async function crash() {
-  throw Error('WUT')
+async function getTodo(ctx: Context) {
+  async function crash() {
+    throw Error('WUT')
+  }
+  logger.info({ body: ctx.request.body }, 'processing body...')
+  await crash()
+  ctx.body = []
 }
 
-router
-  .get('/', async ctx => {
-    logger.info({ body: ctx.request.body }, 'processing body...')
-    await crash()
-    ctx.body = []
-  })
-  .post('/', async ctx => {
-    const todo = await decodeP(Todo, ctx.request.body)
-    if (isDue(todo)) {
-      logger.info({ when: todo.due.getFullYear() }, 'due todo')
-    }
-    ctx.body = todo
-  })
+async function postTodo(ctx: Context) {
+  const todo = await decodeP(Todo, ctx.request.body)
+  if (isDue(todo)) {
+    logger.info({ when: todo.due.getFullYear() }, 'due todo')
+  }
+  ctx.body = todo
+}
