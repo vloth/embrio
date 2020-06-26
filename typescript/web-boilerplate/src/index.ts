af31dae829e3716a403c7f86ad2a97a372980eca
vloth <fran.fhb@gmail.com>
2020-06-26 23:00:22 +0200

    ϋ refactor: Describe UNCAUGHT errors

--- typescript/web-boilerplate/src/index.ts
+++ typescript/web-boilerplate/src/index.ts
@@ -4,14 +4,17 @@ import { app } from './app'
 import { env } from '@adapter/env'
 import { logger } from '@protocol/logger'
 
-process
-  .on('unhandledRejection', reason => {
-    throw reason
-  })
-  .on('uncaughtException', reason => {
-    logger.fatal(reason, 'Exiting application due to an uncaught exception')
-    process.exit(1)
-  })
+process.on('unhandledRejection', rejection => {
+  throw rejection
+})
+
+process.on('uncaughtException', reason => {
+  logger.fatal(
+    reason,
+    '[UNCAUGHT EXCEPTION] Exiting application due to an unhandled exception'
+  )
+  process.exit(1)
+})
 
 app.listen(env.port, () =>
   logger.info('Application running on port %d', env.port)
