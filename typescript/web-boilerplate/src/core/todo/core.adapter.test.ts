1efcc87b82cf38b93a064dc6af87aaaeb6115623
vloth <fran.fhb@gmail.com>
2020-06-28 11:12:32 +0200

    π test: Add tests for todo core adapter

---
+++ typescript/web-boilerplate/src/core/todo/core.adapter.test.ts
@@ -0,0 +1,35 @@
+import { expect } from '@test'
+import * as core from './core.adapter'
+import { decode } from '@adapter/codec/decode'
+
+suite('todo core adapter')
+
+const now = new Date()
+const completedtask = {
+  description: 'do the dishes',
+  done: true,
+  date: now.toISOString()
+}
+
+const pendingtask = {
+  description: 'do the dishes',
+  done: false
+}
+
+test('decode PendingTask', async function () {
+  const decoded = await decode(core.PendingTask, pendingtask)
+  expect(decoded).to.eql(pendingtask)
+})
+
+test('decode CompletedTask', async function () {
+  const decoded = await decode(core.CompletedTask, completedtask)
+  expect(decoded).to.eql({ ...completedtask, date: now })
+})
+
+test('decode Todo', async function () {
+  const completedTaskDecoded = await decode(core.Todo, completedtask)
+  const pendingTaskDecoded = await decode(core.Todo, pendingtask)
+
+  expect(pendingTaskDecoded).to.eql(pendingtask)
+  expect(completedTaskDecoded).to.eql({ ...completedtask, date: now })
+})
