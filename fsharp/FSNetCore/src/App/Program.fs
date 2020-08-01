635edd30755571e1f778006faaee476b53793260
vloth <fran.fhb@gmail.com>
2020-08-01 12:13:23 +0200

    Σ feat: Start fsharp projects yet another time

---
+++ fsharp/FSNetCore/src/App/Program.fs
@@ -0,0 +1,12 @@
+﻿open Library
+
+[<EntryPoint>]
+let main argv =
+    printfn "Nice command-line arguments!
+      Here's what JSON.NET has to say about them:"
+
+    argv
+    |> Array.map getJsonNetJson
+    |> Array.iter (printfn "%s")
+
+    0
